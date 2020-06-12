596c554d908759e1d13568f267d0c372c38380be
febenedet <63066441+febenedet@users.noreply.github.com>
2020-06-12 13:18:45 -0400

    Add files via upload

--- Traffic Source Bid Optimization.sql
+++ Traffic Source Bid Optimization.sql
@@ -1,14 +1,14 @@
 SELECT 
-device_type,
-count(distinct s.website_session_id) as NumberOfSessions,
-count(distinct order_id) as orders,
-count(distinct order_id) / count(distinct s.website_session_id) as SessionToOrderConvRate
-
-FROM mavenfuzzyfactory.website_sessions s
-left join orders o on (s.website_session_id = o.website_session_id)
-where 
-s.created_at < '2012-05-11' and
-utm_source = 'gsearch' and
-utm_campaign = 'nonbrand'
-
-group by device_type
+    device_type,
+    COUNT(DISTINCT s.website_session_id) AS NumberOfSessions,
+    COUNT(DISTINCT order_id) AS orders,
+    COUNT(DISTINCT order_id) / COUNT(DISTINCT s.website_session_id) AS SessionToOrderConvRate
+FROM
+    mavenfuzzyfactory.website_sessions s
+        LEFT JOIN
+    orders o ON (s.website_session_id = o.website_session_id)
+WHERE
+    s.created_at < '2012-05-11'
+        AND utm_source = 'gsearch'
+        AND utm_campaign = 'nonbrand'
+GROUP BY device_type
