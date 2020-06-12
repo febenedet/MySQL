13de432cdff8bdb39c18f3d5fcb972bb55f0f1c7
febenedet <63066441+febenedet@users.noreply.github.com>
2020-06-12 13:08:15 -0400

    Delete Pivoting_Data.sql

--- Pivoting_Data.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-primary_product_id,
-count(distinct case when items_purchased = 1 then order_id else null end) as CountSingleItemOrder,
-count(distinct case when items_purchased = 2 then order_id else null end) as CountTwoItemsOrder
-
-from orders
-
-group by 1
-order by 3 desc
